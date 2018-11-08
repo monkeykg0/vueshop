3bd2c8c8b51b9fe216ab546383bede7e53103707
monkeykg0 <monkeykg0@gmail.com>
2018-11-08 09:37:47 +0800

    首页初始化

---
+++ src/components/pages/ShoppingMall.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="shoppingmall">
+        <!-- search bar layout -->
+        <div class="search-bar">
+            <van-row>
+                <van-col span="3"><img :src="locationIcon"  width="100%" alt=""> </van-col>
+                <van-col span="16">search input</van-col>
+                <van-col span="5">button </van-col>
+            </van-row>  
+        </div>
+            
+    </div>
+</template>
+<script>
+export default {
+    // name:''
+    data(){
+        return {
+            locationIcon: require('../../assets/images/banglocation.png')
+        }
+    }
+}
+</script>
+<style lang="less">
+    .shoppingmall{
+        .search-bar{
+            height:50px;
+            background:#e5017d;
+            line-height:50px;
+        }
+    }
+</style>
+
+
--- src/main.js
+++ src/main.js
@@ -3,7 +3,8 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import {Button,Row,Col} from 'vant'
+Vue.use(Button).use(Row).use(Col)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
